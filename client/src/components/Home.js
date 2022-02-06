25239c608d8db0e9cdc064ed397c3102cd70151e
Alex <kuzminbass@gmail.com>
2022-02-06 13:25:14 +1000

    css

---
+++ client/src/components/Home.js
@@ -0,0 +1,17 @@
+import "../styles/Home.css";
+
+const Home = () => {
+  return (
+    <div className="layout">
+      <div className="traffic-light">
+        <div className="bulb"></div>
+        <div className="bulb"></div>
+      </div>
+      <div className="traffic-light">
+        <div className="bulb"></div>
+        <div className="bulb"></div>
+      </div>
+    </div>
+  );
+};
+export default Home;
