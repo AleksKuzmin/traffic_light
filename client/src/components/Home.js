ae5f565b18696ed362886738d7c7f7460bcd5410
Alex <kuzminbass@gmail.com>
2022-02-06 23:03:36 +1000

    server

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,16 +1,38 @@
 import "../styles/Home.css";
-
+import { useState, useEffect } from "react";
+import Axios from "axios";
 const Home = () => {
+  const [color, SetColor] = useState();
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/status")
+      .then((response) => {
+        SetColor(response.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+  console.log(color);
+
+  useEffect(() => {
+    Axios.put("http://localhost:3001/changeLights").then((response) =>
+      console.log(response.data)
+    );
+  }, []);
+
+  function changeLights() {
+    
+  }
   return (
     <div className="layout">
       <div className="traffic-light">
-        <div className="bulb"></div>
-        <div className="bulb"></div>
+        <div className={"red" + (color === "red" ? " black" : "")}></div>
+        <div className="green" onClick={() => console.log("click")}></div>
       </div>
       <div className="traffic-light">
-        <div className="bulb"></div>
-        <div className="bulb"></div>
+        <div className="red" onClick={() => console.log("click")}></div>
+        <div className="green" onClick={() => console.log("click")}></div>
       </div>
+      <button onClick={changeLights}>Change Traffic Lights</button>
     </div>
   );
 };
