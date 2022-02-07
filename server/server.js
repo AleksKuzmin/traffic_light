638c417c8ec6ba66142476f1dcfaeb7d7752dcd9
Alex <kuzminbass@gmail.com>
2022-02-07 13:27:19 +1000

    server

--- server/server.js
+++ server/server.js
@@ -4,8 +4,24 @@ const app = express();
 const port = 3001;
 
 app.use(cors());
-app.get("/", (req, res) => {
-  res.json({ red: "red", green: "green" });
+let northSouthBound = true;
+const lightsStatus = (req, res) => {
+  let response = {};
+
+  if (northSouthBound) {
+    response.NS = "green";
+    response.WE = "red";
+  } else {
+    response.NS = "red";
+    response.WE = "green";
+  }
+  res.json(response);
+};
+app.get("/status", lightsStatus);
+
+app.put("/changeLights", (req, res) => {
+  northSouthBound = !northSouthBound;
+  lightsStatus(req, res);
 });
 
 app.listen(port, () => {
