dc4a61585cadce65237df9301e31418f79d7cff1
Alex <kuzminbass@gmail.com>
2022-02-07 10:52:28 +1000

    almost done

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,9 @@
 import "../styles/Home.css";
 import { useState, useEffect } from "react";
 import Axios from "axios";
+
 const Home = () => {
-  const [color, SetColor] = useState();
+  const [color, SetColor] = useState([]);
 
   useEffect(() => {
     Axios.get("http://localhost:3001/status")
@@ -11,28 +12,26 @@ const Home = () => {
       })
       .catch((error) => console.log(error));
   }, []);
-  console.log(color);
 
-  useEffect(() => {
-    Axios.put("http://localhost:3001/changeLights").then((response) =>
-      console.log(response.data)
-    );
-  }, []);
-
-  function changeLights() {
-    
+  function change() {
+    Axios.put("http://localhost:3001/changeLights", {}).then((response) => {
+      console.log(response.data);
+    });
   }
+
+  console.log(color.NS);
   return (
     <div className="layout">
       <div className="traffic-light">
-        <div className={"red" + (color === "red" ? " black" : "")}></div>
-        <div className="green" onClick={() => console.log("click")}></div>
+        <div className={color.NS === "green" ? "black" : "red"}></div>
+        <div className={color.NS === "red" ? "black" : "green"}></div>
       </div>
+
       <div className="traffic-light">
-        <div className="red" onClick={() => console.log("click")}></div>
-        <div className="green" onClick={() => console.log("click")}></div>
+        <div className={color.WE === "red" ? "black" : "green"}></div>
+        <div className={color.WE === "green" ? "black" : "red"}></div>
       </div>
-      <button onClick={changeLights}>Change Traffic Lights</button>
+      <button onClick={change}>Change Traffic Lights</button>
     </div>
   );
 };
